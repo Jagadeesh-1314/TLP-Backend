d940173f40b54cf2e798a32900f7189ad73d2fd8
BezawadaVignesh <vigneshbezawada3@gmail.com>
2024-09-12 00:39:36 +0530

    rate limiting and slowing down requests

--- src/login/index.ts
+++ src/login/index.ts
@@ -1,9 +1,15 @@
 import { Router } from "express";
 import loginRouter from "./routes";
+import rateLimit from "express-rate-limit";
 
 const router = Router();
 
 // Defining the core path from which this module should be accessed
+const limiter = rateLimit({
+    windowMs: 20 * 60 * 1000,
+    max: 10,
+  });
+router.use(limiter)
 router.use("/", loginRouter)
 
 export default router;
